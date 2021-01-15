cc7b2154478dc59d28fb7805a1a8d54dff22cd80
robotmischief <robotmischief2020@gmail.com>
2021-01-15 17:00:56 -0300

    feat: first app test

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 //initial setup
+const { request, response } = require('express');
 const express = require('express');
 const fetch = require('node-fetch');
 require('dotenv').config();
@@ -8,8 +9,33 @@ app.listen(port, () => console.log('listening to port ', port));
 //static content
 app.use(express.static('public'));
 
-//fetch Mars data
-app.get('/mars', (request, response) => {
+//fetch Mars weather data
+app.get('/mars', async (request, response) => {
+    //TODO: try catch errors
     const apiKey = process.env.API_NASA_KEY;
-    response.json(apiKey);
+    const url = `https://api.nasa.gov/insight_weather/?api_key=${apiKey}&feedtype=json&ver=1.0`;
+    const data = await fetch(url);
+    const json = await data.json();
+    response.json(json);
+});
+
+//fetch Earth weather location
+app.get('/earth/location/:place', async (request, response) => {
+    //TODO: try catch errors
+    const place = request.params.place;
+    const apiKey = process.env.API_WEATHER_KEY;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=${apiKey}`;
+    const data = await fetch(url);
+    const json = await data.json();
+    response.json(json);
+});
+//fetch Earth weather data
+app.get('/earth/:latlon', async (request, response) => {
+    //TODO: try catch errors
+    const apiKey = process.env.API_WEATHER_KEY;
+    const location = request.params.latlon.split(',');
+    const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${location[0]}&lon=${location[1]}&exclude=current,minutely,hourly,alerts&units=metric&appid=${apiKey}`;
+    const data = await fetch(url);
+    const json = await data.json();
+    response.json(json);
 })
